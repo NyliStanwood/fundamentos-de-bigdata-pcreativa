f9eb1531a9a3e6882a2bd21988072b662a434ba3
Marilyn Stanwood <nyliz@live.com>
2025-12-30 15:01:34 +0100

    edits

--- resources/mstw-setup.sh
+++ resources/mstw-setup.sh
@@ -6,3 +6,10 @@ python3 -m venv env
 source env/bin/activate
 cd $PROJECT_HOME/resources/web
 python3 predict_flask.py
+
+
+
+PWSHll
+
+python -m venv env    # or python3 if that’s your command
+.\env\Scripts\Activate.ps1
